b502ba803763257da696c5ae39830d8232dbff96
mindinpanic <mindinpanic@gmail.com>
2013-01-13 18:21:17 +0200

    Add templates and mock objects to template context.

--- src/app/views.py
+++ src/app/views.py
@@ -2,10 +2,31 @@
 
 __author__ = 'mindinpanic'
 
+
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return u"Index page"
+    user = {
+        'nickname': 'Vova'
+    }
+    posts = [
+        {
+            'author': {'nickname': 'Pechenushka'},
+            'body': u'Моя красота неописуемая'
+        },
+        {
+            'author': {'nickname': 'Vova'},
+            'body': 'Some boring text'
+        }
+    ]
+    return render_template(
+        'index.html',
+        title="Index",
+        user=user,
+        posts=posts
+    )
